cdc806ca75024ed9b85cfead71d7b7e4cd18c9ad
William Jensen <williambjensen01@gmail.com>
2020-09-13 20:33:20 -0600

    Finished cards.js but for some reason the images aren't displaying. When I console.log it shows the images have the correct src path

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,49 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+axios
+	.get('https://lambda-times-backend.herokuapp.com/articles')
+	.then(result => {
+		console.log(result)
+        const data = result.data.articles;
+        Object.values(data).forEach((articles) => {
+            articles.forEach((article) => {
+                cardsContainer.append(createCards(article));
+            })
+        })
+	})
+	.catch(error => {
+		console.log('An error has occurred', error)
+	});
+
+const cardsContainer = document.querySelector('.cards-container')
+
+function createCards(obj) {
+	const card = document.createElement('div');
+	const headline = document.createElement('div');
+	const author = document.createElement('div');
+	const imgContainer = document.createElement('div');
+	const image = document.createElement('img');
+	const name = document.createElement('span');
+
+	card.classList.add('card');
+	headline.classList.add('headline');
+	author.classList.add('author');
+	imgContainer.classList.add('img-container');
+
+    image.src = obj.authorPhoto;
+	
+	headline.textContent = obj.headline;
+    name.textContent = `By ${obj.authorName}`;
+    
+    imgContainer.appendChild(image);
+    author.append(imgContainer, name);
+    card.append(headline, author);
+
+	card.addEventListener('click', () => {
+		console.log(obj.headline)
+	});
+    
+	return card;
+}
